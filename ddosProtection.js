323b02db719192349e4f1228520805a9d9095b03
Volkan <42183076+Pudochu@users.noreply.github.com>
2022-08-29 02:57:27 +0300

    🎉 Big update (Fix's & Optimization's)!
    Recent updates:
    
    -Fixed the problem of obtaining IP address with Request.
    -Fixed the issue of getting log messages even if the log is turned off.
    -Optimization upgraded.
    -The codes for obtaining an IP address have been changed.

--- ddosProtection.js
+++ ddosProtection.js
@@ -1,4 +1,4 @@
-var geoip = require('geoip-country'); //Module (Ram-CPU) optimized. If a major attack occurs, it will not cause any problems.
+var geoip = require('geoip-lite'); //Module (Ram-CPU) optimized. If a major attack occurs, it will not cause any problems.
 
 const userRecently = new Set();
 const usersMap = new Map();
@@ -9,13 +9,12 @@ setInterval(function() {
     if (DDOS_ATTACK == false && ddos > 44) {
         DDOS_ATTACK = true;
         for (let i = 0; i < 20; i++) {
-            console.log('[DEFENSE SYSTEM] WARNING DDOS ATTACK DETECTED!')
+            if (log) console.log('[DEFENSE SYSTEM] WARNING DDOS ATTACK DETECTED!')
         }
         setTimeout(function() {
             for (let i = 0; i < 20; i++) {
-                console.log('[DEFENSE SYSTEM] DDOS ATTACKS NOW STOPPED!')
+                if (log) console.log('[DEFENSE SYSTEM] DDOS ATTACKS NOW STOPPED!')
             }
-
             DDOS_ATTACK = false;
         }, 300 * 1000); // 5 dk;
         return;
@@ -23,19 +22,25 @@ setInterval(function() {
     ddos = 0;
 }, 2 * 1000);
 
+async function ipGet(req) {
+    var ip = (req.headers['x-forwarded-for'] || '').replace(/:\d+$/, '') || req.connection.remoteAddress;
+    if (ip.includes('::ffff:')) {
+        ip = ip.split(':').reverse()[0]
+    }
+    var lookedUpIP = await geoip.lookup(ip);
+    if ((ip === '127.0.0.1' || ip === '::1')) {
+        return "1.11.111.1111"; // If the request comes from localhost, we enter a no-valid ip address.
+    }
+    if (!lookedUpIP) {
+        return "1.11.111.1111"; // If the request comes from localhost, we enter a no-valid ip address.
+    }
+    return lookedUpIP;
+}
+
 module.exports = async (request, main_country, log) => {
-
     try {
-        let ipAddress = request.ipInfo;
-
-        if (ipAddress.error) ipAddress = "1.11.111.1111"; // If the request comes from localhost, we enter a fake ip address.
-        ipAddress = ipAddress.ip
-        var geo = await geoip.lookup(ipAddress);
-        /*if(geo?.country !== "TR") {
-        if(DDOS_ATTACK) return "GLOBAL_DDOS";
-        ddos = ddos + 1;
-        }*/
-
+        let ipAddress = request.headers['cf-connecting-ip'] || await ipGet(request);
+        var geo = request.headers['cf-ipcountry'] || await geoip.lookup(ipAddress).country;
         let getData = await usersMap.get('veri_' + ipAddress) || 0;
         if (getData > 24) {
             if (getData > 99) {
@@ -47,19 +52,19 @@ module.exports = async (request, main_country, log) => {
                     console.log("[DDOS] User Ban Deleted : " + ipAddress)
                     usersMap.set('veri_' + ipAddress, 0)
                 }, 3600 * 1000);
-                console.log("[DDOS] User Banned : " + ipAddress)
+                if (log) console.log("[DDOS] User Banned : " + ipAddress)
                 usersMap.set('veri_' + ipAddress, getData + 999)
                 return "USER_DDOS";
                 return;
             }
-            console.log("[DDOS] User Unlimited Banned : " + ipAddress)
+            if (log) console.log("[DDOS] User Unlimited Banned : " + ipAddress)
             usersMap.set('veri_' + ipAddress, getData + 999)
             return "USER_DDOS";
         } else {
             if (getData) usersMap.set('veri_' + ipAddress, getData + 1)
             if (!getData || getData == 0) {
                 setTimeout(function() {
-                    console.log("[DDOS] User DATA Deleted : " + ipAddress)
+                    if (log) console.log("[DDOS] User DATA Deleted : " + ipAddress)
                     usersMap.set('veri_' + ipAddress, 0)
                 }, 120 * 1000);
                 usersMap.set('veri_' + ipAddress, 1)
@@ -69,8 +74,8 @@ module.exports = async (request, main_country, log) => {
                 ddos = ddos + 1;
             }
         }
-        if (log) console.log("[DDOS-LOG] Joined site : " + geo?.country + " | DOS-Count : " + getData + "/25 | Global-DOS: " + ddos + "/45")
-        return false;
+    if (log) console.log("[DDOS-LOG] Joined site : " + geo + " | DOS-Count : " + getData + "/25 | Global-DOS: " + ddos + "/45")
+    return false;
     } catch (e) {
         console.log(e)
     }
